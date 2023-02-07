1e1439e6a766ed70ad1b9304dbdf824c8f56ba67
ueneid <eidhie@gmail.com>
2023-02-07 17:34:12 +0900

    Day11 part2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,16 +1,16 @@
 class Day11 {
 
     class Monkey(
-        var itemList: MutableList<Int>,
+        var itemList: MutableList<Long>,
         var operationPair: Pair<Char, String>,
-        var divible: Int,
+        var divisible: Long,
         var destMonkeys: List<Int>,
     ) {
-        var inspectCount = 0
+        var inspectCount = 0L
 
-        fun operate(worryLevel: Int): Int {
+        fun operate(worryLevel: Long): Long {
             val (op, opValue) = operationPair
-            val v = if (opValue == "old") worryLevel else opValue.toInt()
+            val v = if (opValue == "old") worryLevel else opValue.toLong()
             return when (op) {
                 '+' -> worryLevel + v
                 '-' -> worryLevel - v
@@ -20,8 +20,8 @@ class Day11 {
             }
         }
 
-        fun isDivisible(worryLevel: Int): Boolean {
-            return worryLevel % divible == 0
+        fun isDivisible(worryLevel: Long): Boolean {
+            return worryLevel % divisible == 0L
         }
     }
 
@@ -41,11 +41,11 @@ class Day11 {
                 val lines = listOf(
                     IO.readStr(), IO.readStr(), IO.readStr(), IO.readStr(), IO.readStr()
                 ).map { it.trim() }
-                val itemList = regexList[0].matchEntire(lines[0])?.groups?.get(1)?.value?.split(", ")?.map { it.toInt() }?.toMutableList()
+                val itemList = regexList[0].matchEntire(lines[0])?.groups?.get(1)?.value?.split(", ")?.map { it.toLong() }?.toMutableList()
                     ?: throw IllegalArgumentException()
                 val (op, value) = regexList[1].matchEntire(lines[1])?.destructured
                     ?: throw IllegalArgumentException()
-                val divValue = regexList[2].matchEntire(lines[2])?.groups?.get(1)?.value?.toInt()
+                val divValue = regexList[2].matchEntire(lines[2])?.groups?.get(1)?.value?.toLong()
                     ?: throw IllegalArgumentException()
                 val trueMonkey = regexList[3].matchEntire(lines[3])?.groups?.get(1)?.value?.toInt()
                     ?: throw IllegalArgumentException()
@@ -61,7 +61,7 @@ class Day11 {
         return input
     }
 
-    fun solve1(input: List<Monkey>): Int {
+    fun solve1(input: List<Monkey>): Long {
         repeat(20) {
             for ((i, monkey) in input.withIndex()) {
                 while (monkey.itemList.isNotEmpty()) {
@@ -81,14 +81,32 @@ class Day11 {
         return top2[0] * top2[1]
     }
 
-    fun solve2(input: List<Monkey>): Int {
-        return 1
+    fun solve2(input: List<Monkey>): Long {
+        // I totally don't understand the purpose of this part, so referred https://yonatankarp.com/advent-of-code-2022-day-11-kotlin-edition
+        val commonMultiple = input.map { monkey -> monkey.divisible }.reduce(Long::times)
+        repeat(10000) {
+            for ((i, monkey) in input.withIndex()) {
+                while (monkey.itemList.isNotEmpty()) {
+                    val item = monkey.itemList.removeFirst()
+                    monkey.inspectCount++
+                    val multiplied = monkey.operate(item)
+                    val newLevel = multiplied % commonMultiple
+                    if (monkey.isDivisible(newLevel)) {
+                        input[monkey.destMonkeys[0]].itemList.add(newLevel)
+                    } else {
+                        input[monkey.destMonkeys[1]].itemList.add(newLevel)
+                    }
+                }
+            }
+        }
+        val top2 = input.sortedByDescending { it.inspectCount }.take(2).map { it.inspectCount }
+        return top2[0] * top2[1]
     }
 }
 
 fun main() {
     val obj = Day11()
     val input = obj.getInputs()
-    println(obj.solve1(input))
-//    println(obj.solve2(input).joinToString("\n"))
+//    println(obj.solve1(input))
+    println(obj.solve2(input))
 }
