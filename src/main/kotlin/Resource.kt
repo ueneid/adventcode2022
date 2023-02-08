dfac5516aa81dda6a7ec8632eca9ed36834e9245
ueneid <eidhie@gmail.com>
2023-02-08 13:10:52 +0900

    Change to `internal object` from `class` and a bit fix

--- src/main/kotlin/Resource.kt
+++ src/main/kotlin/Resource.kt
@@ -1,17 +1,20 @@
 import java.io.File
 import java.net.URI
 
-class Resource {
+internal object Resource {
     fun resourceAsText(fileName: String): String =
-        File(fileName.toURI()).readText()
+        File(fileName.toURI()).readText().trim()
 
     fun resourceAsListOfString(fileName: String): List<String> =
         File(fileName.toURI()).readLines()
+
+    fun resourceAsListOfBunchOfString(fileName: String, separator: String = "\n\n"): List<String> =
+        resourceAsText(fileName).split(separator)
 
     fun resourceAsListOfInt(fileName: String): List<Int> =
         resourceAsListOfString(fileName).map { it.toInt() }
 
     private fun String.toURI(): URI =
-        this.javaClass.classLoader.getResource(this)?.toURI()
+        Resource.javaClass.classLoader.getResource(this)?.toURI()
             ?: throw IllegalArgumentException("Cannot find Resource: $this")
 }
